25d61c076f6d924cd90b7ef289212c30602acc19
diegomrch <diegoraudya@gmail.com>
2026-01-26 20:55:07 +0700

    Update 2_Tentang_Sistem.py

--- pages/2_Tentang_Sistem.py
+++ pages/2_Tentang_Sistem.py
@@ -42,7 +42,9 @@ berdasarkan data yang tersimpan pada **Supabase Database**.
 ---
 
 ### 👨‍🎓 Pengembang
-**Nama:** Diego Raudya Trimeidianto  
+ARIO YUDHANTO - 714252018
+DIEGO RAUDYA TRIMEIDIANTO - 714252004
 **Program Studi: D4 Teknik Informatika RPL**  
 **Institusi:** Universitas Logistik dan Bisnis Internasional (ULBI)
 """)
+
